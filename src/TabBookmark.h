d86346e5ca5f470f62ea82b071f4f4d49f3dfc34
kfsboss <kfsboss@163.com>
2022-10-08 16:07:06 +0800

    Update TabBookmark.h

--- src/TabBookmark.h
+++ src/TabBookmark.h
@@ -429,21 +429,21 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
             }
 
             // 双击关闭
-            if (isOnOneTab)
-            {
-                if (isOnlyOneTab)
-                {
-                    //DebugLog(L"keep_tab");
-                    //ExecuteCommand(IDC_NEW_TAB, hwnd);
-                    ExecuteCommand(IDC_NEW_TAB);
-                    ExecuteCommand(IDC_SELECT_PREVIOUS_TAB);
-                    ExecuteCommand(IDC_CLOSE_TAB);
-                }
-                else
-                {
-                    ExecuteCommand(IDC_CLOSE_TAB);
-                }
-            }
+            // if (isOnOneTab)
+            // {
+            //     if (isOnlyOneTab)
+            //     {
+            //         //DebugLog(L"keep_tab");
+            //         //ExecuteCommand(IDC_NEW_TAB, hwnd);
+            //         ExecuteCommand(IDC_NEW_TAB);
+            //         ExecuteCommand(IDC_SELECT_PREVIOUS_TAB);
+            //         ExecuteCommand(IDC_CLOSE_TAB);
+            //     }
+            //     else
+            //     {
+            //         ExecuteCommand(IDC_CLOSE_TAB);
+            //     }
+            // }
         }
 
         if (wParam == WM_MBUTTONUP)
